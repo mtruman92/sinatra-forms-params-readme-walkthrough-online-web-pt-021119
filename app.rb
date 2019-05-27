233c250d2a3a4d685a1288c630efe4bcf3ebee2a
Mickae Truman <mtruman@nyit.edu>
2019-05-27 06:03:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  
+  post '/food' do
+    params.to_s
+  end
+  
 post '/food' do
   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
 end
